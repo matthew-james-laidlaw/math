e3f5a073a235f96908ad99dfb0eaf8c974509c02
Matt <121856602+matthew-james-laidlaw@users.noreply.github.com>
2025-10-05 08:29:56 +0000

    add identity simplification and some tests for it

--- src/Simplifier.h
+++ src/Simplifier.h
@@ -47,7 +47,35 @@ auto TryIdentityRules(Expression*& expr) -> bool
         return false;
     }
 
-    return true;
+    auto lhs_lit = dynamic_cast<Literal*>(bin->left);
+    auto rhs_lit = dynamic_cast<Literal*>(bin->right);
+
+    if (bin->op.type == Token::Type::Plus)
+    {
+        bool additive_identity_left = lhs_lit && lhs_lit->value == 0.0;
+        bool additive_identity_right = rhs_lit && rhs_lit->value == 0.0;
+        if (additive_identity_left) { expr = bin->right; return true; }
+        if (additive_identity_right) { expr = bin->left; return true; }
+    }
+    else if (bin->op.type == Token::Type::Minus)
+    {
+        bool subtraction_identity = rhs_lit && rhs_lit->value == 0.0;
+        if (subtraction_identity) { expr = bin->left; return true; }
+    }
+    else if (bin->op.type == Token::Type::Star)
+    {
+        bool multiplicative_identity_left = lhs_lit && lhs_lit->value == 1.0;
+        bool multiplicative_identity_right = rhs_lit && rhs_lit->value == 1.0;
+        if (multiplicative_identity_left) { expr = bin->right; return true; }
+        if (multiplicative_identity_right) { expr = bin->left; return true; }
+    }
+    else if (bin->op.type == Token::Type::Slash)
+    {
+        bool division_identity = rhs_lit && rhs_lit->value == 1.0;
+        if (division_identity) { expr = bin->left; return true; }
+    }
+
+    return false;
 }
 
 auto SimplifyStep(Expression*& expr) -> bool
--- tests/SimplifierTests.cpp
+++ tests/SimplifierTests.cpp
@@ -16,3 +16,75 @@ TEST(SimplifierTests, ConstantFolding)
     EXPECT_NE(lit, nullptr);
     EXPECT_EQ(lit->value, 3.0);
 }
+
+TEST(SimplifierTests, AdditiveIdentityLeft)
+{
+    auto source = "0 + x"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseAdditiveOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
+
+TEST(SimplifierTests, AdditiveIdentityRight)
+{
+    auto source = "x + 0"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseAdditiveOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
+
+TEST(SimplifierTests, SubtractionIdentity)
+{
+    auto source = "x - 0"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseAdditiveOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
+
+TEST(SimplifierTests, MultiplicativeIdentityLeft)
+{
+    auto source = "1 * x"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseMultiplicativeOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
+
+TEST(SimplifierTests, MultiplicativeIdentityRight)
+{
+    auto source = "x * 1"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseMultiplicativeOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
+
+TEST(SimplifierTests, DivisionIdentity)
+{
+    auto source = "x / 1"s;
+    auto ast = Parser(Lexer(source).Lex()).ParseAdditiveOperation();
+
+    SimplifyStep(ast);
+
+    auto var = dynamic_cast<Variable*>(ast);
+    EXPECT_NE(var, nullptr);
+    EXPECT_EQ(var->identifier, "x");
+}
